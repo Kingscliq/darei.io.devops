27509153f22a48a7497952048d18a4279c412fef
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2025-05-02 20:47:05 -0700

    chore: adding comments to my bash scripts

---
+++ comments/single_line_comments.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Create directories
+mkdir Folder1
+mkdir Folder2
+mkdir Folder3
+
+# Create users
+sudo useradd user1
+sudo useradd user2
+sudo useradd user3
+
+# This is a single-line comment in Bash
+echo "Hello, you are learning Bash Scripting on DAREY.IO!" # This is also a comment, following a command
+
